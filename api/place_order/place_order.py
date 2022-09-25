9f6fa8b149c6fe0d88c0918fe618464aa782fb5b
theianchia <theianchia@users.noreply.github.com>
2022-09-25 11:52:24 +0800

    Setup sms pub sub

--- api/place_order/place_order.py
+++ api/place_order/place_order.py
@@ -3,9 +3,9 @@ from flask_cors import CORS
 import os, sys
 import requests
 from invokes import invoke_http
-import amqp_setup
 import pika
 import json
+from send_sms import send_sms
 
 app = Flask(__name__)
 CORS(app)
@@ -20,51 +20,24 @@ def hello():
 
 @app.route("/place_order", methods=['POST'])
 def place_order():
-  if request.is_json:
-    try:
-      order = request.get_json()
-      result = processPlaceOrder(order)
-      return jsonify(result), result["code"]
+  data = request.get_json()
+  res = invoke_http(
+    order_URL + '/add-order',
+    method='POST',
+    json=data,
+  )
 
-    except Exception as e:
-      exc_type, exc_obj, exc_tb = sys.exc_info()
-      fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-      ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
-
-      return jsonify({
-        "code": 500,
-        "message": "place_order.py internal error: " + ex_str
-      }), 500
-
-  return jsonify({
-    "code": 400,
-    "message": "Invalid JSON input: " + str(request.get_data())
-  }), 400
-
-
-def processPlaceOrder(order):
-  order_result = invoke_http(order_URL, method='POST', json=order)
-
-  code = order_result["code"]
-  message = json.dumps(order_result)
-
-  if code not in range(200, 300):
-    print('\n\n-----Publishing the (order error) message with routing_key=order.error-----')
-
-    amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.error", body=message, properties=pika.BasicProperties(delivery_mode = 2))     
-    print("\nOrder status ({:d}) published to the RabbitMQ Exchange:".format(code), order_result)
-
-    return {
-      "code": 500,
-      "data": {"order_result": order_result},
-      "message": "Order creation failure sent for error handling."
+  if res["code"] in range(200, 300):
+    routing_key = 'order.info'
+    message = f"Hi {data['user_name']}, your order was successfully placed!"
+    data = {
+      'body': message,
+      'to': data['phone_number']
     }
 
-  else:
-    print('\n\n-----Publishing the (order info) message with routing_key=order.info-----')        
-    amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.info",body=message)
-  
-  print("\nOrder published to RabbitMQ Exchange.\n")
+    msg_status = send_sms(data)
+
+  return jsonify(res), res["code"]
 
 
 if __name__ == "__main__":
--- api/place_order/send_sms.py
+++ api/place_order/send_sms.py
@@ -8,11 +8,13 @@ def send_sms(message):
   amqp_setup.check_setup()
   sms_message = json.dumps(message)
 
+  print(sms_message)
+
   amqp_setup.channel.basic_publish(
       exchange=amqp_setup.exchangename, 
       routing_key="order.sms", 
       body=sms_message, 
       properties=pika.BasicProperties(delivery_mode = 2)
-  ) 
+  )
 
   return
